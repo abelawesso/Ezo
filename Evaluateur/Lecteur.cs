23555f2ca7e74379f1b7d326ffe76e3c745e565d
Mazama Awesso <mazawesso@otr.tg>
2026-06-18 23:30:53 -0400

    Ajout des classes Lecteur et Ordonnanceur (analyse/RPN)
    Ajout de Lecteur.cs pour l’analyse lexicale d’expressions mathématiques (nombres, opérateurs, fonctions) avec gestion des erreurs de syntaxe. Ajout de Ordonnanceur.cs pour la conversion en notation respectant les priorités des opérateurs

---
+++ Evaluateur/Lecteur.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    internal class Lecteur
+    {
+        private static readonly HashSet<string> Fonctions = new(StringComparer.OrdinalIgnoreCase)
+        {
+            "sqrt", "sin", "cos", "tan", "log"
+        };
+
+        private static readonly HashSet<char> Operateurs = new() { '+', '-', '*', '/' };
+   
+        public List<TypeElementInfo> Lire(string expression) 
+        {
+            var elements = new List<TypeElementInfo>();
+            int i = 0;
+            expression = expression.Trim();
+
+            while(i< expression.Length)
+            {
+                char c = expression[i];
+                if(char.IsWhiteSpace(c))
+                {
+                    i++;
+                    continue;
+                }
+                if(char.IsDigit(c) || c== '.' || (c== '-' && EstMoinsUnaire(elements)))
+                {
+                    elements.Add(LireNombre(expression, ref i));
+                }
+                else if(char.IsLetter(c))
+                {
+                    elements.Add(LireFonction(expression, ref i));
+                }
+                else if(Operateurs.Contains(c))
+                {
+                    elements.Add(new TypeElementInfo(TypeElement.Operateur, c.ToString()));
+                    i++;
+                }
+                else
+                {
+                    throw new ExpressionInvalideException($"Caractère invalide: {c}");
+                }
+            }
+
+            return elements;
+        }
+
+        private static bool EstMoinsUnaire(List<TypeElementInfo> elements)
+        {
+            if (elements.Count == 0)
+                return true;
+            var dernierElement = elements.Last();
+            return dernierElement.Type == TypeElement.Operateur ||
+                   dernierElement.Type == TypeElement.ParentheseOuvrante;
+        }
+
+        private static TypeElementInfo LireNombre(string expression, ref int index)
+        {
+            int debut = index, pointDecimalTrouve = 0;
+            if (expression[index] == '-')
+            {
+                index++;
+            }
+
+            while (index < expression.Length && (char.IsDigit(expression[index]) || expression[index] == '.'))
+            {
+                if (expression[index] == '.' && ++pointDecimalTrouve > 1)
+                {                  
+                        throw new ExpressionInvalideException("Nombre invalide: plusieurs points décimaux trouvés.");
+                }
+                index++;
+            }
+            return new TypeElementInfo(TypeElement.Nombre, expression[debut..index]);
+        }
+   
+        private static TypeElementInfo LireFonction(string expression, ref int index)
+        {
+            int debut = index;
+            while (index < expression.Length && char.IsLetter(expression[index]))
+            {
+                index++;
+            }
+            string nomFonction = expression[debut..index];
+            if (!Fonctions.Contains(nomFonction))
+            {
+                throw new ExpressionInvalideException($"Fonction inconnue: {nomFonction}");
+            }
+            return new TypeElementInfo(TypeElement.Fonction, nomFonction);
+        }
+    
+    }
+}
---
+++ Evaluateur/Ordonnanceur.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core
+{
+    internal class Ordonnanceur
+    {
+        private static readonly Dictionary<string, (int Priorite, bool Gauche)> Ordre = new(StringComparer.OrdinalIgnoreCase)
+        {
+            { "sqrt", (3, false) },
+            { "sin", (3, false) },
+            { "cos", (3, false) },
+            { "tan", (3, false) },
+            { "log", (3, false) },
+            { "*", (2, true) },
+            { "/", (2, true) },
+            { "+", (1, true) },
+            { "-", (1, true) }
+        };
+
+        public static Queue<TypeElementInfo> Ordonner(List<TypeElementInfo> elements)
+        {
+            var sortie = new Queue<TypeElementInfo>();
+            var pile = new Stack<TypeElementInfo>();
+
+            foreach (var element in elements)
+            {
+                switch (element.Type)
+                {
+                    case TypeElement.Nombre:
+                        sortie.Enqueue(element);
+                        break;
+                    case TypeElement.Fonction:
+                    case TypeElement.Operateur:                       
+                        EmpilerOperateur(element, sortie, pile);
+                        break;
+                    case TypeElement.ParentheseOuvrante:
+                        pile.Push(element);
+                        break;
+                    case TypeElement.ParentheseFermante:
+                        TraiterParentheseFermante(sortie, pile);
+                        break;
+                }
+            }
+            while (pile.Count > 0)
+            {
+                if (pile.Peek().Type == TypeElement.ParentheseOuvrante || pile.Peek().Type == TypeElement.ParentheseFermante)
+                    throw new ExpressionInvalideException("Parenthèses non équilibrées");
+                sortie.Enqueue(pile.Pop());
+            }
+            return sortie;
+        }
+    
+        private static void EmpilerOperateur(TypeElementInfo elementInfo, Queue<TypeElementInfo> sortie, Stack<TypeElementInfo> pile)
+        {
+            var (priorite, gauche) = Ordre[elementInfo.Valeur];
+
+            while (pile.Count > 0 &&
+                              (pile.Peek().Type == TypeElement.Fonction || pile.Peek().Type == TypeElement.Operateur) &&
+                              ((Ordre[pile.Peek().Valeur].Priorite > Ordre[elementInfo.Valeur].Priorite) ||
+                               (Ordre[pile.Peek().Valeur].Priorite == Ordre[elementInfo.Valeur].Priorite && Ordre[elementInfo.Valeur].Gauche)))
+            {
+                sortie.Enqueue(pile.Pop());
+            }
+            pile.Push(elementInfo);
+        }
+
+        private static void TraiterParentheseFermante(Queue<TypeElementInfo> sortie, Stack<TypeElementInfo> pile)
+        {
+            while (pile.Count > 0 && pile.Peek().Type != TypeElement.ParentheseOuvrante)
+            {
+                sortie.Enqueue(pile.Pop());
+            }
+            if (pile.Count == 0 || pile.Peek().Type != TypeElement.ParentheseOuvrante)
+                throw new ExpressionInvalideException("Parenthèses non équilibrées");
+            pile.Pop(); // Retirer la parenthèse ouvrante
+        }
+    }
+}
