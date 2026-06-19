35475861bfdac828b305c8f22e167c5ee0a8d3a9
Mazama Awesso <mazawesso@otr.tg>
2026-06-18 21:41:26 -0400

    AJout des classes d'exceptions

---
+++ Exceptions/ExpressionInvalideException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Exceptions
+{
+    public class ExpressionInvalideException : Exception
+    {
+        public ExpressionInvalideException() : base("Expression invalide")
+        {
+        }
+    }
+}
